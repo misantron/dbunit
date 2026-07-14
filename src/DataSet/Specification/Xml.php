13e4a3d08716c43e981681e8077476a84b2be6f1
a.o.ivanov <misantron@gmail.com>
2026-07-14 15:24:26 +0300

    ECS rules apply

--- src/DataSet/Specification/Xml.php
+++ src/DataSet/Specification/Xml.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace PHPUnit\DbUnit\DataSet\Specification;
 
 use PHPUnit\DbUnit\DataSet\XmlDataSet;
